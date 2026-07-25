d28d302bd1442f8c8a6764c23804fb92ed8c272c
Shihab177 <shihab7777h@gmail.com>
2026-07-25 18:46:08 +0600

    add:  category and popular image

---
+++ assets/index.ts
@@ -0,0 +1,22 @@
+import category_image_one from "./images/categories/category_image-1.png";
+import category_image_two from "./images/categories/category_image-2.png";
+import category_image_three from "./images/categories/category_image-3.png";
+import category_image_four from "./images/categories/category_image-4.png";
+import popular_image_one from "./images/popular/popular_image-1.png";
+import popular_image_two from "./images/popular/popular_image-2.png";
+import popular_image_three from "./images/popular/popular_image-3.png";
+import popular_image_four from "./images/popular/popular_image-4.png";
+import popular_image_five from "./images/popular/popular_image-5.png";
+
+export {
+    category_image_four,
+    category_image_one,
+    category_image_three,
+    category_image_two,
+    popular_image_five,
+    popular_image_four,
+    popular_image_one,
+    popular_image_three,
+    popular_image_two
+};
+
